71c09dfe8483342be388a1893729705003b76bd1
FedeGauna16 <fede.face.gauna@gmail.com>
2022-04-08 12:42:42 -0300

    a

--- envio de formulario/index.js
+++ envio de formulario/index.js
@@ -10,14 +10,14 @@ const d = document,
       $span.textContent = el.title;
       $span.classList.add("contact-form-error", "none");
       el.insertAdjacentElement("afterend", $span);
-   })
+   });
 
    d.addEventListener("keyup", e =>{
       if(e.target.matches(".contact-form [required]")){
          let input = e.target,
          pattern = input.pattern || input.dataset.pattern;
 
-         if(pattern){
+         if(pattern && input.value != ""){
             let regex = new RegExp(pattern);
             return !regex.exec(input.value)
                ? d.getElementById(input.name).classList.add("is-active")
@@ -26,7 +26,28 @@ const d = document,
          }
          
          if(!pattern){
-
+            return input.value == ""
+               ? d.getElementById(input.name).classList.add("is-active")
+               : d.getElementById(input.name).classList.remove("is-active")
          }
       }
-   })
+   });
+
+   d.addEventListener("submit", e =>{
+      e.preventDefault();
+      alert("Enviando");
+     
+      const $loader = d.querySelector(".contact-form-loader"),
+         $response = d.querySelector(".contact-form-response");
+         
+      $loader.classList.remove("none");
+
+      setTimeout(() => {
+         $loader.classList.add("none");
+         $response.classList.remove("none");
+         $form.reset();
+
+         setTimeout(() => $response.classList.add("none"), 3000);
+      }, 3000)
+
+   });
